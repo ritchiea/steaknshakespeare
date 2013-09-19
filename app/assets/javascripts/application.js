2944f4b1b30361fce73ffb6abd7953ba4ef82b67
Andrew Ritchie <ritchiea@gmail.com>
2013-09-18 23:06:00 -0400

    generated rails app, added ember

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require ./vendor/handlebars-1.0.0
+//= require ./vendor/ember.js
+//= require ./vendor/ember-data.js
+//= require_tree ./lib
+//= require app/app
+//= require_tree ./app/models
+//= require_tree ./app/controllers
+//= require_tree ./app/views
+//= require_tree ./app/helpers
+//= require_tree ./app/templates
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Steaknshakespeare::Application.config.secret_key_base = '8efb0e761917cc03a054d9c8cab21d885394bed7f201f1c3e6da6d82b606b5a13c54c912e753696561db5aeeea1ab1f750e5d74e36f68c4cd208822a9183a8f0'
